6166b69beda63f721d58239a3d89c54ca81f728a
Theo Bullock <theodore.bullockjr@gmail.com>
2026-07-27 18:31:59 -0400

    Add Terraform project foundation

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,1 +1,11 @@
+variable "aws_region" {
+  description = "AWS region where resources will be deployed."
+  type        = string
+  default     = "us-east-1"
+}
 
+variable "project_name" {
+  description = "Project name used for tagging AWS resources."
+  type        = string
+  default     = "aws-down-server-recovery"
+}
